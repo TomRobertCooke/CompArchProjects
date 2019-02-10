e2e23c567955c710027e5a67dfd622868e964d40
TomRobertCooke <46798275+TomRobertCooke@users.noreply.github.com>
2019-02-09 22:06:31 -0500

    Update third.c

--- PA1/third/third.c
+++ PA1/third/third.c
@@ -9,12 +9,41 @@ typedef struct node
 
 int insert(node** table, int num)
 {
-
+    if (table[num % 10000] == 0)
+    {
+        table[num % 10000] = malloc(sizeof(node));
+        table[num % 10000]->data = num;
+        table[num % 10000]->next = NULL;
+    } else
+    {
+        node* ptr = table[num % 10000];
+        while (ptr->next != NULL)
+        {
+            ptr = ptr->next;
+        }
+        ptr->next = malloc(sizeof(node));
+        ptr->next->data = num;
+        ptr->next->next = NULL;
+        return 1;
+    }
+    return 0;
 }
 
 int search(node** table, int num)
 {
-
+    if (table[num % 10000] != 0)
+    {
+        node* ptr = table[num % 10000];
+        while (ptr != NULL)
+        {
+            if (ptr->data == num)
+            {
+                return 1;
+            }
+            ptr = ptr->next;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char** argv) /* mod tablesize is the hash function function that we will be using */
@@ -25,13 +54,15 @@ int main(int argc, char** argv) /* mod tablesize is the hash function function t
 		return 0;
 	}
 
-	int collisions = 0;
+	int collisions, successes;
+    collisions = 0;
+    successes = 0;
 	node** table = calloc(10000, sizeof(node*));
 
 	char mode;
 	int data;
 	FILE* filename = fopen(argv[1], "r");
-	if (fscanf(filename, "%c %d", &mode, &data) <= 0)
+	if (fscanf(filename, "%c %d", &mode, &data) > 0)
 	{
 		if (mode == 'i')
 		{
@@ -39,9 +70,37 @@ int main(int argc, char** argv) /* mod tablesize is the hash function function t
 			{
 				collisions++;
 			}
-		} else ()
+		} else if (mode == 's')
+        	{
+            		if (search() == 1)
+            		{
+                		successes++;
+            		}
+        	}
+	} else
+    {
+        printf("0\n0\n");
+        return 0;
+    }
 
-	}
+    while (fscanf(filename, "\n%c %d", &mode, &data) > 0)
+    {
+        if (mode == 'i')
+		{
+			if (insert(table, data) == 1)
+			{
+				collisions++;
+			}
+		} else if (mode == 's')
+        {
+            if (search(table, data) == 1)
+            {
+                successes++;
+            }
+        }
+    }
 
-	return 0;
+    printf("%d\n%d\n", collisions, successes);
+
+    return 0;
 }
